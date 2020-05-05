cc798c50689eef97bfc6f8c716ee855f3a7c89f0
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-05-05 15:13:56 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    currentList: [],
+    newList: [],
+  };
+
+  newItem = (event) => {
+    this.setState({
+      currentList: event.target.value,
+    });
+  };
+
+  addToList = () => {
+    this.setState({
+      newList: [...this.state.newList, this.state.currentList],
+    });
+  };
+  render() {
+    const Lists = this.state.newList.map((list) => {
+      return <h1>{list}</h1>;
+    });
+    return (
+      <div>
+        <h1>React Todo List</h1>
+        <input onChange={(event) => this.newItem(event)} type='text' />
+        <button onClick={this.addToList}>Add</button>
+        {Lists}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/addTodo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const addTodo = () => {
+  return (
+    <div>
+      <h3>Add todo</h3>
+    </div>
+  );
+};
+
+export default addTodo;
