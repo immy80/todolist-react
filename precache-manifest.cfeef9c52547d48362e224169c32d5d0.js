8bad2cf6773b3af7e1077d36e9298cf64d337682
= <codenation@Codes-MacBook-Pro.local>
2020-05-06 13:43:34 +0100

    Updates

---
+++ precache-manifest.cfeef9c52547d48362e224169c32d5d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f872c37d2251a243134c3e39b8fe1590",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "926adcf32b27dd72d4a2",
+    "url": "/todolist-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d2be067313777c2b9e3d",
+    "url": "/todolist-react/static/js/2.a34c0749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-react/static/js/2.a34c0749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "926adcf32b27dd72d4a2",
+    "url": "/todolist-react/static/js/main.b494243c.chunk.js"
+  },
+  {
+    "revision": "4d5a234e1ba4f783e08e",
+    "url": "/todolist-react/static/js/runtime-main.f9fe5cfc.js"
+  }
+]);
